37ffe976e29926812352f70e95d84145ab027d99
jmjoy <jmjoy@apache.org>
2022-10-24 12:01:14 +0800

    Add get_command.

--- src/plugin/plugin_memcached.rs
+++ src/plugin/plugin_memcached.rs
@@ -4,8 +4,13 @@ use crate::{
     context::RequestContext,
     execute::{get_this_mut, AfterExecuteHook, BeforeExecuteHook, Noop},
 };
+use anyhow::{bail, Context};
 use once_cell::sync::Lazy;
+use phper::{functions::call, values::ExecuteData};
 use skywalking::skywalking_proto::v3::SpanLayer;
+use tracing::warn;
+
+// TODO Detect is error.
 
 static MEC_KEYS_COMMANDS: Lazy<Vec<String>> = Lazy::new(|| {
     [
@@ -132,39 +137,86 @@ impl MemcachedPlugin {
         let function_name = function_name.to_owned();
         (
             Box::new(move |request_id, execute_data| {
-                let mut peer = "".to_owned();
-                if MEC_STR_KEYS_COMMANDS.contains(&function_name.to_ascii_lowercase()) {
-                    execute_data.get_parameter(0);
-                }
+                let peer = if MEC_STR_KEYS_COMMANDS.contains(&function_name.to_ascii_lowercase()) {
+                    let mut f = || {
+                        let key = {
+                            let key = execute_data.get_parameter(0);
+                            if !key.get_type_info().is_string() {
+                                bail!("The argument key of {} isn't string", &function_name);
+                            }
+                            key.clone()
+                        };
+                        let this = get_this_mut(execute_data)?;
+                        let info = this.call(&"getServerByKey".to_ascii_lowercase(), [key])?;
+                        let info = info.as_z_arr().context("Server isn't array")?;
+                        let host = info
+                            .get("host")
+                            .context("Server host not exists")?
+                            .as_z_str()
+                            .context("Server host isn't string")?
+                            .to_str()?;
+                        let port = info
+                            .get("port")
+                            .context("Server port not exists")?
+                            .as_long()
+                            .context("Server port isn't long")?;
+                        Ok::<_, anyhow::Error>(format!("{}:{}", host, port))
+                    };
+                    match f() {
+                        Ok(peer) => peer,
+                        Err(err) => {
+                            warn!(?err, "Get peer failed");
+                            "".to_owned()
+                        }
+                    }
+                } else {
+                    "".to_owned()
+                };
 
                 let span = RequestContext::try_with_global_ctx(request_id, |ctx| {
                     let mut span =
-                        ctx.create_exit_span(&format!("{}->{}", class_name, function_name), peer);
+                        ctx.create_exit_span(&format!("{}->{}", class_name, function_name), &peer);
                     span.with_span_object_mut(|obj| {
                         obj.set_span_layer(SpanLayer::Cache);
                         obj.component_id = COMPONENT_PHP_MEMCACHED_ID;
                         obj.add_tag("db.type", "memcached");
+
+                        match get_command(execute_data, &function_name) {
+                            Ok(cmd) => {
+                                obj.add_tag("memcached.command", cmd);
+                            }
+                            Err(err) => {
+                                warn!(?err, "get command failed");
+                            }
+                        }
                     });
                     Ok(span)
                 });
-
-                // let handle = get_this_mut(execute_data)?.handle();
-
-                // debug!(handle, function_name, "call PDO method");
-
-                // let mut span = with_dsn(handle, |dsn| {
-                //     create_exit_span_with_dsn("PDO", &function_name, dsn)
-                // })?;
-
-                // if execute_data.num_args() >= 1 {
-                //     if let Some(statement) = execute_data.get_parameter(0).as_z_str() {
-                //         span.add_tag("db.statement", statement.to_str()?);
-                //     }
-                // }
 
                 Ok(Box::new(span) as _)
             }),
             Noop::noop(),
         )
     }
+}
+
+fn get_command(execute_data: &mut ExecuteData, function_name: &str) -> anyhow::Result<String> {
+    let num_args = execute_data.num_args();
+    let mut items = Vec::with_capacity(num_args + 1);
+    items.push(function_name.to_owned());
+
+    for i in 0..num_args {
+        let parameter = execute_data.get_parameter(i);
+        let s = if parameter.get_type_info().is_array() {
+            let result = call("json_encode", [parameter.clone()])?;
+            result.expect_z_str()?.to_str()?.to_string()
+        } else {
+            let mut parameter = parameter.clone();
+            parameter.convert_to_string();
+            parameter.expect_z_str()?.to_str()?.to_string()
+        };
+        items.push(s)
+    }
+
+    Ok(items.join(" "))
 }
