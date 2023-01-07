8ad379fa9bcbf7ddaf36712cfc20bfb838c26868
jmjoy <jmjoy@apache.org>
2023-01-07 11:28:07 +0800

    Fix php-fpm freeze after large amount of request. (#39)

--- src/worker.rs
+++ src/worker.rs
@@ -32,7 +32,7 @@ use tokio::{
     runtime::{self, Runtime},
     select,
     signal::unix::{signal, SignalKind},
-    sync::mpsc,
+    sync::mpsc::{self, error::TrySendError},
     time::sleep,
 };
 use tonic::{
@@ -153,9 +153,11 @@ async fn start_worker(server_addr: String) {
                                     Ok(i) => Ok(i),
                                 };
 
-                                if let Err(err) = tx.send(r).await {
+                                if let Err(err) = tx.try_send(r) {
                                     error!(?err, "Send failed");
-                                    return;
+                                    if !matches!(err, TrySendError::Full(_)) {
+                                        return;
+                                    }
                                 }
                             }
                         });
