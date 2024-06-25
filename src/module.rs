5317ef37ed07811e76b60c5ae1773f4afb3fdf71
LeiZhang-Hunter <357733652@qq.com>
2024-06-26 01:06:15 +0800

    add token

--- src/module.rs
+++ src/module.rs
@@ -82,7 +82,7 @@ pub static TOKEN_NAME: Lazy<String> = Lazy::new(|| {
         .and_then(|s| s.to_str().ok())
         .unwrap_or_default();
 
-    defined_token_name.to_string()
+    defined_token_name.to_string()k
 });
 
 pub static SKYWALKING_VERSION: Lazy<i64> =
